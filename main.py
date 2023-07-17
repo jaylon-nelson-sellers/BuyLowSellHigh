6290bf7437c19a3f3c85cb2b61a6b67028933b97
Jaylon Nelson-Sellers <jcns@uw.edu>
2023-07-16 22:04:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,27 +19,23 @@ def get_stock_info(tickers):
         data[ticker] = (current_price, low_52_week, high_52_week)
     return data
 
+from datetime import datetime
+
 def get_closest_prices(stocks):
     # calculate the difference between current price and high/low
-    diff_high = {key: round(abs(value[0] - value[2]), 2) for key, value in stocks.items()}
-    diff_low = {key: round(abs(value[0] - value[1]), 2) for key, value in stocks.items()}
+    diff_high = {key: abs(value[0] / value[2]) for key, value in stocks.items()}
 
-    # sort the dictionary by the difference to high and low, get top 10
+    # sort the dictionary by the difference to high and low, get top 20
     closest_to_high = sorted(diff_high.items(), key=lambda x: x[1])[:10]
-    closest_to_low = sorted(diff_low.items(), key=lambda x: x[1])[:10]
-
     # prepare the content to write into the text file
-    content = "Best Sells:\n"
-    content += "\n".join([f"{x[0]} - C: {round(stocks[x[0]][0], 2)}, 52H: {round(stocks[x[0]][2], 2)}, dif: {round(stocks[x[0]][0]- stocks[x[0]][2], 2)}" for x in closest_to_high])
+    content = "Best Buys:\n"
+    content += "\n".join([f"{x[0]} - C: {round(stocks[x[0]][0], 2)}, 52H: {round(stocks[x[0]][2], 2)}, discount: {round(1 - (stocks[x[0]][0] / stocks[x[0]][2]), 2)}%" for x in closest_to_high])
 
-    content += "\n\n Best Buys:\n"
-    content += "\n".join([f"{x[0]} - C: {round(stocks[x[0]][0], 2)}, 52L: {round(stocks[x[0]][1], 2)}, dif: {round(stocks[x[0]][0] -stocks[x[0]][1], 2)}"  for x in closest_to_low])
-
+    print(content)
     # write the content into a text file
     current_date = datetime.now().strftime('%Y-%m-%d')
     with open(f'stock_prices_{current_date}.txt', 'w') as f:
         f.write(content)
-
 
 tickers = read_ticker_file("t.txt")
 stocks = get_stock_info(tickers)
